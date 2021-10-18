9608642923054c543e31dbe80a0b455fd3a8d57f
Akshay Kakatkar <akshayakakatkaraa@gmail.com>
2021-10-18 17:47:27 +0200

    [REVERTME] sm7250-common: Drop bootjars
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Change-Id: I5874c6a663bb3f128a28f11b486f5c272b6eb4bc

--- common.mk
+++ common.mk
@@ -375,11 +375,11 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # OnePlus
-PRODUCT_PACKAGES += \
-    oneplus-fwk.oneplus_lito
+#PRODUCT_PACKAGES += \
+#    oneplus-fwk.oneplus_lito
 
-PRODUCT_BOOT_JARS += \
-    oneplus-fwk.oneplus_lito
+#PRODUCT_BOOT_JARS += \
+#    oneplus-fwk.oneplus_lito
 
 # Power
 PRODUCT_PACKAGES += \
@@ -428,8 +428,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+#    telephony-ext
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
@@ -512,8 +512,8 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
 
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
