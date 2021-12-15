dbad32cc6a817d3a4bfa1063e82f0e143fb18b28
johnmart19 <johnivan19999@gmail.com>
2021-12-15 21:53:06 +0800

    sm7250-common: Add missing android.hardware.media.omx
    Fixes: android.hardware.media.omx@1.0-service: Service not available yet
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Change-Id: I2d802a4bb58c9a407a43c14ab8a91d9668601591

--- common.mk
+++ common.mk
@@ -391,6 +391,7 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-service \
     init.qti.media.sh \
     libOmxAacEnc \
     libOmxAmrEnc \
@@ -400,7 +401,8 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_omx.vendor
 
 # OnePlus
 PRODUCT_PACKAGES += \
