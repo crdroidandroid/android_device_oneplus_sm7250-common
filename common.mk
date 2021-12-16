manikantaraavi/device_oneplus_sm7250-common-proton

sm7250-common: Switch to udfps

common.mk
@@ -268,7 +268,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.oneplus \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus
+    android.hardware.biometrics.fingerprint@2.3-service.oneplus \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 # GPS
 PRODUCT_COPY_FILES += \

crdroidandroid/android_device_oneplus_sm7250-common

Signed-off-by: firebird11 <hbgassel@gmail.com>
Change-Id: Ibd4b0e715aa1ef8498964edeb736fa12d857c938

common.mk
@@ -269,9 +269,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.oneplus_lito \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_lito \
-    vendor.oneplus.fingerprint.extension@1.0 \
-    vendor.oneplus.hardware.display@1.0
+    android.hardware.biometrics.fingerprint@2.3-service.oneplus_lito \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 # GPS
 PRODUCT_COPY_FILES += \
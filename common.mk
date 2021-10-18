bf44536245e0086194fd3c9c9a10295445e56d31
TheScarastic <warabhishek@gmail.com>
2021-10-18 17:38:40 +0200

    sm7250-common: Switch to udfps
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Change-Id: Ibd4b0e715aa1ef8498964edeb736fa12d857c938
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- common.mk
+++ common.mk
@@ -269,9 +269,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.oneplus_lito \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_lito \
-    vendor.oneplus.fingerprint.extension@1.0 \
-    vendor.oneplus.hardware.display@1.0
+    android.hardware.biometrics.fingerprint@2.3-service.oneplus_lito \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 # GPS
 PRODUCT_COPY_FILES += \
