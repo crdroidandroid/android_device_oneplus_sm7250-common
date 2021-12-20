d7f191831bca42d061ab4c4cd80abea708a179ac
firebird11 <hbgassel@gmail.com>
2021-12-20 16:01:54 +0200

    Revert "sm7250-common: Switch to udfps"
    This reverts commit bf44536245e0086194fd3c9c9a10295445e56d31.

--- common.mk
+++ common.mk
@@ -270,8 +270,9 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.oneplus_lito \
-    android.hardware.biometrics.fingerprint@2.3-service.oneplus_lito \
-    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_lito \
+    vendor.oneplus.fingerprint.extension@1.0 \
+    vendor.oneplus.hardware.display@1.0
 
 # GPS
 PRODUCT_COPY_FILES += \
