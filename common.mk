671c970f26b8e172ff83a5ff3e3af5a0cf3436f4
chandu078 <chandudyavanapelli03@gmail.com>
2022-01-02 04:51:34 +0000

    sm7250-common: rro_overlays: Add RRO for OnePlusCamera & OnePlusGallery
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Change-Id: If69bfa3d417207b36bf08a0e87f8ab97916f0fbe
    Signed-off-by: c79 <vexed@riseup.net>

--- common.mk
+++ common.mk
@@ -265,10 +265,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
-# Doze
-PRODUCT_PACKAGES += \
-    OnePlusDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
@@ -413,6 +409,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     oneplus-fwk
+
+# OnePlus Apps
+PRODUCT_PACKAGES += \
+    OnePlusCameraOverlay \
+    OnePlusGalleryOverlay
 
 # Power
 PRODUCT_PACKAGES += \
