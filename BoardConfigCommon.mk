manikantaraavi/device_oneplus_sm7250-common-proton

sm7250-common: Adjust for recent tri state key changes
tri-state-key package has been integrated into KeyHandler app, and
KeyHandler itself no longer needs to be added to lineage-sdk overlays.

Change-Id: I13b8f05e8e0b036c9303a01ec0c7e2d99b6756f7

BoardConfigCommon.mk
@@ -175,6 +175,7 @@ VENDOR_SECURITY_PATCH := 2021-09-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot

crdroidandroid/android_device_oneplus_sm7250-common

BoardConfigCommon.mk
@@ -180,6 +180,7 @@ VENDOR_SECURITY_PATCH := 2021-08-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot