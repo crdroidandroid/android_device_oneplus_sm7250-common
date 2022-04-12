1bdf257b6ea270bfa2b6bca3306c61c4f1dbde92
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2022-04-12 15:56:38 +0530

    sm7250-common: Update Blobs from OOS 11.0.16.16.EB13DA
    Signed-off-by: c79 <vexed@riseup.net>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -181,7 +181,7 @@ CUSTOM_APNS_FILE := $(COMMON_PATH)/configs/apns-conf.xml
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-01-01
+VENDOR_SECURITY_PATCH := 2022-03-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
