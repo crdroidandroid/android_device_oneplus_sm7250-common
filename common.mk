d05ad5f9c4cea4a4fc7c3a19176786d9f2c95ab7
aswin7469 <aswinas@pixysos.com>
2022-04-15 11:11:07 +0530

    sm7250-common: remove skia reduceOpsTaskSplitting
    * enabled by default on 12L
    
    This reverts commit 074cc126ebff286c25f95f6212604a06fce959b1.
    
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- common.mk
+++ common.mk
@@ -580,10 +580,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.disable_rescue=true
 endif
 
-# Enable skia reduceOpsTaskSplitting
-PRODUCT_PROPERTY_OVERRIDES += \
-    renderthread.skia.reduceopstasksplitting=true
-
 # Media
 PRODUCT_PACKAGES += \
     libmm-omxcore \
