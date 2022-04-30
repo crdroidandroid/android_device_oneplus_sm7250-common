b352573b3730787c74bcb71251ced4573d535772
c79 <vexed@riseup.net>
2022-05-01 01:04:15 +0300

    Revert "sm7250-common: Build sound_trigger.primary.lito from source"
    * We are using a new CAF tag, we need prebuilts
    
    This reverts commit 7e3b7f298e4a960da22eb3e0e8d8b5173be89b8c.

--- common.mk
+++ common.mk
@@ -131,7 +131,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.3-impl \
-    sound_trigger.primary.lito \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.lito \
