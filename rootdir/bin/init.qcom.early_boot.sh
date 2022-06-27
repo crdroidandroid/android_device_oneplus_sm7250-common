13725d8371b4fdaee9392492dad1d770fd9e92d9
Chandu Dyavanapelli <chandudyavanapelli03@gmail.com>
2022-06-27 13:20:14 +0530

    sm7250-common: rootdir: Update scripts from LA.UM.9.12.r1-14200-SMxx50.QSSI13.0
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: rany <ranygh@riseup.net>

--- rootdir/bin/init.qcom.early_boot.sh
+++ rootdir/bin/init.qcom.early_boot.sh
@@ -1,6 +1,6 @@
 #! /vendor/bin/sh
 
-# Copyright (c) 2012-2013,2016,2018-2020 The Linux Foundation. All rights reserved.
+# Copyright (c) 2012-2013,2016,2018-2021 The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -370,9 +370,7 @@ case "$target" in
                 setprop vendor.display.enhance_idle_time 1
                 setprop vendor.netflix.bsp_rev ""
                 ;;
-            *)
-                # default case is for bengal
-                setprop vendor.netflix.bsp_rev "Q6115-31409-1"
+             518)
                 ;;
         esac
         ;;
